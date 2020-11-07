6ef581ecc27a587ac9989928569e92aa943b844e
Maher El Gamil <maherbusnes@gmail.com>
2020-11-07 21:55:06 +0200

    Create an endpoint to handle GET requests  for all available categories.

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -27,11 +27,18 @@ def create_app(test_config=None):
       return response
 
   '''
-  @TODO:
   Create an endpoint to handle GET requests
   for all available categories.
   '''
+  @app.route('/categoriess', methods=['GET'])
+  def get_categories():
+      categories = [category.format() for category in Category.query.all()]
 
+      return jsonify({
+          "success": True,
+          "categories": categories,
+          "total_categories": len(categories)
+      })
 
   '''
   @TODO:
